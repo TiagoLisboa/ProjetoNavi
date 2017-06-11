244d8592efb16fa2fc2ac4c71c34acfc78f644c0
Tiago Lisboa <tiago.caio.ol@gmail.com>
2017-06-11 08:21:20 -0300

    created main layout

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
+	<!-- Custom styles for this template -->
+    <link href="/css/app.css" rel="stylesheet">
+</head>
+<body>
+
+	<div class="container">
+		<div class="row">
+			
+			@yield ('content')
+
+		</div>
+	</div>
+
+</body>
+</html>
