fb699ab22841b9e079b68254d321306c2eda5982
Tiago Lisboa <tiago.caio.ol@gmail.com>
2017-06-11 15:56:29 -0300

    added integration with ViaCEP API resolved issue #1

--- public/js/app.js
+++ public/js/app.js
@@ -11040,6 +11040,20 @@ $(document).ready(function () {
 			$('input#other').prop('disabled', false);
 		}
 	});
+
+	$("input[name=cep]").change(function () {
+		var cep = $(this).val();
+		cep = cep.replace('-', '');
+
+		if (cep.length == 8) {
+			$.get('https://viacep.com.br/ws/' + cep + '/json/', function (data) {
+				$('input#address').val(data.logradouro + ", " + data.unidade);
+				$('input#neighbourhood').val(data.bairro);
+				$('input#city').val(data.localidade);
+				$('input#uf').val(data.uf);
+			});
+		}
+	});
 });
 
 /***/ }),
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,11 +3,27 @@ require('./../../../node_modules/jquery-mask-plugin/dist/jquery.mask.min.js');
 
 require('./bootstrap');
 
+
 $(document).ready(function () {
 	$("input[name=sex]").change(function () {
 		$( 'input#other' ).prop('disabled', true)
 		if ($('input#other_r').prop ('checked')) {
 			$( 'input#other' ).prop('disabled', false)
+		}
+	});
+
+	$("input[name=cep]").change(function () {
+		let cep = $( this ).val();
+		cep = cep.replace('-', '');
+		
+		if (cep.length == 8) 
+		{
+			$.get('https://viacep.com.br/ws/' + cep + '/json/', function (data) {
+				$('input#address').val(data.logradouro + ", " + data.unidade)
+				$('input#neighbourhood').val(data.bairro)
+				$('input#city').val(data.localidade)
+				$('input#uf').val(data.uf)
+			});
 		}
 	})
 });
